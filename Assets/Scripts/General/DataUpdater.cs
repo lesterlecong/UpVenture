30f7ee0dcdbcab86c93d9bac8b5714d6d4d161e9
lester_lecong <hgg.lester.lecong@gmail.com>
2015-06-26 07:49:04 +0800

    add Data Updater script

---
+++ Assets/Scripts/General/DataUpdater.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System.Collections;
+using AssemblyCSharp;
+
+using Couchbase.Lite;
+using Couchbase.Lite.Unity;
+using Newtonsoft;
+
+public class DataUpdater : MonoBehaviour {
+	public GameObject couchbaseDatabaseObject;
+
+	private CouchbaseDatabase couchbaseDatabase;
+	private Replication pushReplication;
+	private Replication pullReplication;
+	private UserDefineKeys defineKeys;
+
+	void Start () {
+		couchbaseDatabase = (CouchbaseDatabase)couchbaseDatabaseObject.GetComponent (typeof(CouchbaseDatabase));
+		pullReplication = couchbaseDatabase.GetPullReplication ();
+		pushReplication = couchbaseDatabase.GetPushReplication ();
+		StartCoroutine (UpdateProgress ());
+	}
+	
+	IEnumerator UpdateProgress(){
+		pullReplication.Start ();
+		pushReplication.Start ();
+
+		while ((pullReplication.Status == ReplicationStatus.Active) || (pushReplication.Status == ReplicationStatus.Active)){
+
+			yield return new WaitForSeconds(1.0f);
+		}
+
+		Application.LoadLevel (PlayerPrefs.GetString (defineKeys.NextScene));
+
+	}
+}
