4b2789ffc01bcef016da17e8c68da018b2a6e52a
lester_lecong <hgg.lester.lecong@gmail.com>
2015-06-29 07:38:37 +0800

    add delay in calling replication

--- Assets/Scripts/General/DataUpdater.cs
+++ Assets/Scripts/General/DataUpdater.cs
@@ -1,39 +1,61 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
 using System.Collections;
 using AssemblyCSharp;
 
 using Couchbase.Lite;
 using Couchbase.Lite.Unity;
 using Newtonsoft;
-using PreviewLabs;
 
 public class DataUpdater : MonoBehaviour {
 	public GameObject couchbaseDatabaseObject;
+	public Text logText;
 
 	private CouchbaseDatabase couchbaseDatabase;
 	private Replication pushReplication;
 	private Replication pullReplication;
-	private UserDefineKeys defineKeys;
+
+
 
 	void Start () {
+		logText.text += "Start Data Updater\n";
 		couchbaseDatabase = (CouchbaseDatabase)couchbaseDatabaseObject.GetComponent (typeof(CouchbaseDatabase));
 		pullReplication = couchbaseDatabase.GetPullReplication ();
 		pushReplication = couchbaseDatabase.GetPushReplication ();
-		StartCoroutine (UpdateProgress ());
-	}
-	
-	IEnumerator UpdateProgress(){
 		pullReplication.Start ();
-		pushReplication.Start ();
+		Invoke ("StartReplicate", 0.1f);
+	}
 
+	void StartReplicate(){
+		logText.text += "Replication Start\n";
+
+
+		bool isOffline = (pullReplication.Status == ReplicationStatus.Offline);
+
+		if (!isOffline) {
+			logText.text += "Sync Gateway is Online\n";
+			pushReplication.Start ();
+			StartCoroutine (UpdateProgress ());
+		} else {
+			logText.text += "Sync Gateway is Offline\n";
+			NextScene();
+		}
+	}
+
+	IEnumerator UpdateProgress(){
+		logText.text += "Update Progress";	
 		while ((pullReplication.Status == ReplicationStatus.Active) || (pushReplication.Status == ReplicationStatus.Active)){
-
+			logText.text += ".";	
 			yield return new WaitForSeconds(1.0f);
 		}
-
-		Application.LoadLevel (NextSceneNameHolder.nextScene);
-
+		logText.text += "\n";	
+		NextScene ();
 	}
+
+	void NextScene(){
+		UserDefineKeys defineKeys;
+		logText.text += "Next Scene: " + PlayerPrefs.GetString (defineKeys.NextScene);
+		Application.LoadLevel(PlayerPrefs.GetString(defineKeys.NextScene));
+	}
+
 }
