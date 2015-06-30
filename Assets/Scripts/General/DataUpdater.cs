0caa94eab137c39dbee9a065a4b9963801c704f5
lester_lecong <hgg.lester.lecong@gmail.com>
2015-07-01 07:36:58 +0800

    Fix problem on couchbase crashing issue

--- Assets/Scripts/General/DataUpdater.cs
+++ Assets/Scripts/General/DataUpdater.cs
@@ -18,38 +18,16 @@ public class DataUpdater : MonoBehaviour {
 
 
 	void Start () {
-		logText.text += "Start Data Updater\n";
+
 		couchbaseDatabase = (CouchbaseDatabase)couchbaseDatabaseObject.GetComponent (typeof(CouchbaseDatabase));
-
-		if ( couchbaseDatabase == null) {
-			logText.text += "Couchbase is null \n";
-		} else {
-			logText.text += "Couchbase is not null \n";
-		}
-		//couchbaseDatabase.InitializeReplication ();
-		logText.text += "Database is" + (couchbaseDatabase.IsDatabaseNull() ? " indeed" : " not") + " null" + "\n";
-		logText.text += "Document is" + (couchbaseDatabase.IsDocumentNull() ? " indeed" : " not") + " null" + "\n";
-		logText.text += "Replications are" + (couchbaseDatabase.ReplicationsNull() ? " indeed" : " not") + " null" + "\n";
-
 		pullReplication = couchbaseDatabase.GetPullReplication ();
-		logText.text += "Pull Replication already get \n";
 		pushReplication = couchbaseDatabase.GetPushReplication ();
-		logText.text += "Push Replication already get \n";
-
-		if (pullReplication == null || pushReplication == null) {
-			logText.text += "Replication is null \n";
-		} else {
-			logText.text += "Replication is not null \n";
-		}
-		pullReplication.Start ();
-
-
+		
 		Invoke ("StartReplicate", 0.1f);
 	}
 
 	void StartReplicate(){
-		logText.text += "Replication Start\n";
-
+		pullReplication.Start ();
 
 		bool isOffline = (pullReplication.Status == ReplicationStatus.Offline);
 
@@ -75,7 +53,6 @@ public class DataUpdater : MonoBehaviour {
 
 	void NextScene(){
 		UserDefineKeys defineKeys;
-		logText.text += "Next Scene: " + PlayerPrefs.GetString (defineKeys.NextScene);
 		Application.LoadLevel(PlayerPrefs.GetString(defineKeys.NextScene));
 	}
 
