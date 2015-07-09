66bcfbfa47a52b31fc2ceae416da54ca72c11e94
lester_lecong <hgg.lester.lecong@gmail.com>
2015-07-10 05:07:27 +0800

    add class to invoke home menu scene after the splash screen

---
+++ Assets/Scripts/Menu Scripts/HomeMenuInvoker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HomeMenuInvoker : MonoBehaviour {
+
+	public string homeMenuSceneName ="home menu";
+
+	void Start(){
+		Invoke ("InvokeHomeMenu", 2.0f);
+	}
+
+
+	void InvokeHomeMenu(){
+		Application.LoadLevel (homeMenuSceneName);
+	}
+}
