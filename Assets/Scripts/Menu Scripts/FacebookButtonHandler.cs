0661093ac07d55303336b95af2c88c30e5f730d6
lester_lecong <hgg.lester.lecong@gmail.com>
2015-07-24 07:00:56 +0800

    add FacebookButtonHandler script for adding listener during click

---
+++ Assets/Scripts/Menu Scripts/FacebookButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using AssemblyCSharp;
+
+public class FacebookButtonHandler : MonoBehaviour {
+
+	private GameObject socialMediaHandlerObject;
+	private SocialMediaHandler socialMediaHandler;
+
+	void Awake(){
+		SetupSocialMediaHandler ();
+		SetButtonListener ();
+	}
+
+	void SetupSocialMediaHandler(){
+		socialMediaHandlerObject = GameObject.Find ("SocialMediaHandlerObject");
+		if (socialMediaHandlerObject != null) {
+			socialMediaHandler = (SocialMediaHandler) socialMediaHandlerObject.GetComponent(typeof(SocialMediaHandler));
+			if(socialMediaHandler != null){
+				socialMediaHandler.socialMediaType = SocialMediaType.FACEBOOK;
+			}
+		}
+	}
+
+	void SetButtonListener(){
+		Button facebookButton = transform.GetComponent<Button> ();
+		facebookButton.onClick.AddListener (() => {
+			LoginFacebook (); 
+		});
+	}
+	
+	void LoginFacebook(){
+		socialMediaHandler.Login ();
+	}
+}
