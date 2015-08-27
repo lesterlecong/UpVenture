e12d6d2f8ae1c1330690e4f4872c5def238998c7
lester_lecong <hgg.lester.lecong@gmail.com>
2015-08-27 21:34:45 +0800

    Add Class for handling sound of birds

---
+++ Assets/Scripts/General/BirdSoundWhenOnCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdSoundWhenOnCamera : MonoBehaviour {
+	
+	public AudioClip birdSound;
+
+	private AudioSource audioSource;
+
+	void Start(){
+		audioSource = gameObject.GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Bird") {
+			ActivateSound(birdSound);
+
+			//ActivateSound(birdFlap);
+
+		}
+	}
+
+	void ActivateSound(AudioClip audioClip){
+		audioSource.clip = audioClip;
+		audioSource.Play ();
+	}
+
+	void DeactivateSound(){
+		audioSource.Stop ();
+	}
+	
+}
