3756092c44d7db3d0fa78124a9ee9bccc09bc8ff
lester_lecong <hgg.lester.lecong@gmail.com>
2015-08-31 15:10:00 +0800

    add Checkbox controller

---
+++ Assets/Scripts/Menu Scripts/CheckboxController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using AssemblyCSharp;
+
+public class CheckboxController : MonoBehaviour {
+
+	public Toggle soundeffectsToggle;
+
+	private static bool soundeffectsStatus = true;
+
+	void Start(){
+		soundeffectsToggle.isOn = (PlayerPrefs.GetInt (GameSystemDefineKeys.SoundFXState) == 1);
+	}
+
+	public void SoundEffectsSettings(){
+		PlayerPrefs.SetInt (GameSystemDefineKeys.SoundFXState, (soundeffectsToggle.isOn)? 1:0);
+	}
+}
